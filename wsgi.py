9066d2809562bb55998b4b8349c1021268ae3fdb
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-09-12 14:16:19 -0400

    troubleshoot gunicorn startup

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from app import app
 # do some production specific things to the app
 app.config['DEBUG'] = False
 
-if __name__ == "__main__": 
-        app.run() 
+#if __name__ == "__main__": 
+#        app.run() 
