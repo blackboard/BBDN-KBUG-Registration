32604ba9ec32bec1634804ecfaa5d65ea8e20b08
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-09-12 14:00:30 -0400

    Initial commit

---
+++ RestUserController.py
@@ -0,0 +1,132 @@
+import json
+from cachetools import TTLCache
+import requests
+import datetime
+import time
+import ssl
+import sys
+import os
+import urllib.parse
+
+import Config
+
+
+
+class RestUserController():
+    target_url = ''
+
+    def __init__(self, target_url, token):
+        
+        self.target_url = target_url
+        self.token = token
+        
+        if Config.config['verify_certs'] == 'True':
+            self.verify_certs = True
+        else:
+            self.verify_certs = False
+
+        self.user_info = None
+
+    def getUserInfo(self):
+        return(self.user_info)
+
+    def createLearnUser(self,user):
+        endpoint = 'https://' + self.target_url + '/learn/api/public/v1/users'
+
+        print('[User:createLearnUser()] token: ' + self.token)
+        #"Authorization: Bearer $token"
+        authStr = 'Bearer ' + self.token
+        print('[User:createLearnUser()] authStr: ' + authStr)
+        
+
+        print("[User:createLearnUser()] POST Request URL: " + endpoint)
+        print("[User:createLearnUser()] JSON Payload: NONE REQUIRED")
+        r = requests.post(endpoint, json=user, headers={'Authorization':authStr, 'Content-Type' : 'application/json'},  verify=self.verify_certs)
+
+        print("[User:createLearnUser()] STATUS CODE: " + str(r.status_code) )
+        #print("[User:getUser()] RESPONSE:" + str(r.text))
+        if r.status_code == 201 and r.text:
+            res = json.loads(r.text)
+            print(json.dumps(res,indent=4, separators=(',', ': ')))
+            return(res)
+        elif r.status_code == 409:
+            res = self.updateLearnUser(user)
+            return(res)
+        else:
+            return({ "http_status" : r.status_code, "result" : r.text })
+    
+    def updateLearnUser(self,user):
+        endpoint = 'https://' + self.target_url + '/learn/api/public/v1/users/userName:' + user['userName']
+
+        print('[User:createLearnUser()] token: ' + self.token)
+        #"Authorization: Bearer $token"
+        authStr = 'Bearer ' + self.token
+        print('[User:createLearnUser()] authStr: ' + authStr)
+        
+
+        print("[User:createLearnUser()] POST Request URL: " + endpoint)
+        print("[User:createLearnUser()] JSON Payload: NONE REQUIRED")
+        r = requests.patch(endpoint, json=user, headers={'Authorization':authStr, 'Content-Type' : 'application/json'},  verify=self.verify_certs)
+
+        print("[User:createLearnUser()] STATUS CODE: " + str(r.status_code) )
+        #print("[User:getUser()] RESPONSE:" + str(r.text))
+        if r.status_code == 200 and r.text:
+            res = json.loads(r.text)
+            print(json.dumps(res,indent=4, separators=(',', ': ')))
+            return(res)
+        else:
+            return({ "http_status" : r.status_code, "result" : r.text })
+
+    def enrollUserInAllCourses(self, userId):
+        
+        for id in range(82,97):
+            courseId = "_" + str(id) + "_1"
+            
+            endpoint = 'https://' + self.target_url + '/learn/api/public/v1/courses/' + courseId + '/users/' + userId
+
+            print('[User:createLearnUser()] token: ' + self.token)
+            #"Authorization: Bearer $token"
+            authStr = 'Bearer ' + self.token
+            print('[User:createLearnUser()] authStr: ' + authStr)
+            
+            payload = {
+                "availability": {
+                    "available": "Yes"
+                },
+                "courseRoleId": "Student"
+            }
+
+            print("[User:createLearnUser()] POST Request URL: " + endpoint)
+            print("[User:createLearnUser()] JSON Payload: NONE REQUIRED")
+            r = requests.put(endpoint, json=payload, headers={'Authorization':authStr, 'Content-Type' : 'application/json'},  verify=self.verify_certs)
+
+            print("[User:createLearnUser()] STATUS CODE: " + str(r.status_code) )
+            #print("[User:getUser()] RESPONSE:" + str(r.text))
+            if r.status_code == 201 and r.text:
+                res = json.loads(r.text)
+                print(json.dumps(res,indent=4, separators=(',', ': ')))
+            else:
+                print("Error registering user <" + userId +"> for session <" + courseId + ">")
+
+    def getUserInfoFromLearn(self):
+        OAUTH_URL = 'https://' + self.target_url + '/learn/api/public/v1/users/me'
+
+        print('[User:getUser()] token: ' + self.token)
+        #"Authorization: Bearer $token"
+        authStr = 'Bearer ' + self.token
+        print('[User:getUser()] authStr: ' + authStr)
+        
+
+        print("[User:getUser()] GET Request URL: " + OAUTH_URL)
+        print("[User:getUser()] JSON Payload: NONE REQUIRED")
+        r = requests.get(OAUTH_URL, headers={'Authorization':authStr},  verify=self.verify_certs)
+
+        print("[User:getUser()] STATUS CODE: " + str(r.status_code) )
+        #print("[User:getUser()] RESPONSE:" + str(r.text))
+        if r.text:
+            res = json.loads(r.text)
+            print(json.dumps(res,indent=4, separators=(',', ': ')))
+            self.user_info = res
+            return(self.user_info)
+        else:
+            print("NONE")
---
+++ app.py
@@ -0,0 +1,148 @@
+import datetime
+import os
+import pprint
+
+from flask import Flask, jsonify, request, render_template, url_for
+from flask_caching import Cache
+from werkzeug.exceptions import Forbidden
+import json
+from flask.wrappers import Response
+
+import Config
+
+import RestAuthController, RestUserController
+import string
+import secrets
+
+from multiprocessing import Process
+from flask_wtf import form
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+app = Flask('bbdn-biab-registration', template_folder='templates', static_folder='static')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+cache = Cache(app)
+
+PAGE_TITLE = 'Kansas BUG Registration'
+
+basic_oauth_service = None
+
+class Password():
+    def generate(self):
+        
+        alphabet = string.ascii_letters + string.digits
+        while True:
+            password = ''.join(secrets.choice(alphabet) for i in range(10))
+            if (any(c.islower() for c in password)
+                    and any(c.isupper() for c in password)
+                    and sum(c.isdigit() for c in password) >= 3):
+                break
+        
+        return password
+
+def init_service():
+    
+    global basic_oauth_service
+
+    try:
+        basic_oauth_service = RestAuthController.RestAuthController()
+        basic_oauth_service.setBasicToken()
+
+    except Exception as e:
+        print(str(e))
+
+
+
+@app.route('/', methods=['GET'])
+def registration():
+    tpl_kwargs = {
+        'page_title': PAGE_TITLE
+    }
+    
+    return render_template('registration.html')
+
+@app.route('/register/', methods=['POST'])
+def register():
+        form = request.form
+
+        print(str(form))
+
+        given_name = form['fname']
+        family_name = form['lname']
+        email = form['email']
+        company = form['institution']
+        track = form['track']
+        job_title = form['title']
+        
+        password = Password().generate()
+
+        heavy_process = Process(  # Create a daemonic process with heavy "my_func"
+            target=executeRegistration, args=(given_name,family_name,email,company,job_title,track,password),
+            daemon=True
+        )
+        heavy_process.start()
+
+        tp_kwargs = {
+            'page_title' : "Registration Confirmed!",
+            'url': 'https://' + Config.config['learn_rest_url'],
+            'username' : email,
+            'password' : password
+        }
+        
+        return render_template('confirmation.html', **tp_kwargs)
+
+
+def executeRegistration(given_name,family_name,email,company,job_title,track,password):
+    
+    global basic_oauth_service
+    token = basic_oauth_service.getBasicToken()
+
+    
+    userCtrl = RestUserController.RestUserController(Config.config['learn_rest_url'], token)
+
+    user = {
+        "userName": email,
+        "password": password,
+        "institutionRoleIds" : [
+            "kbug"
+        ],
+        "availability": {
+            "available": "Yes"
+        },
+        "name": {
+            "given": given_name,
+            "family": family_name
+        },
+        "job": {
+            "title": job_title,
+            "company": company
+        },
+        "contact": {
+            "email": email
+        }
+    }
+
+    devres = userCtrl.createLearnUser(user)
+
+    enrres = userCtrl.enrollUserInAllCourses("userName:" + email)
+
+if __name__ == '__main__':
+    
+    print("call init_service")
+
+    init_service()
+
+    print("init_service completed, start app")
+    
+    port = int(os.environ.get('PORT', 9001))
+    app.run(host='0.0.0.0', port=port)
