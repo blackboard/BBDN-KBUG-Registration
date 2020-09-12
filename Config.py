aa5949a3d7038c117797a4df83436e7ff46759df
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-09-12 14:01:08 -0400

    Initial commit

---
+++ Config.py
@@ -0,0 +1,7 @@
+config = {
+    "verify_certs" : "True",
+    "learn_rest_url" : "devcon.blackboard.com",
+    "learn_rest_key" : "aa0948dd-275e-4002-ba3e-f6c8fb6f98bf",
+    "learn_rest_secret" : "MlMEbnpJR9C1E0ObEUXZMrGrtSzJLa6d",
+    "app_url" : "https://devcon-leaderboard.herokuapp.com"
+}
