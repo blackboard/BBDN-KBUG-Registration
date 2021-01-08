afed2e49819bf01540198692c6fc261ddebd6e7c
Scott Hurrey <Scott.Hurrey@blackboard.com>
2021-01-08 16:33:34 -0500

    Initial commit

--- Config.py
+++ Config.py
@@ -3,5 +3,5 @@ config = {
     "learn_rest_url" : "devcon.blackboard.com",
     "learn_rest_key" : "aa0948dd-275e-4002-ba3e-f6c8fb6f98bf",
     "learn_rest_secret" : "MlMEbnpJR9C1E0ObEUXZMrGrtSzJLa6d",
-    "app_url" : "https://devcon-leaderboard.herokuapp.com"
+    "app_url" : "https://mibug21-registration.herokuapp.com"
 }
